17195d4d5b45147b18a9321aedafd432fb39290d
evanlour <138373709+evanlour@users.noreply.github.com>
2024-06-07 02:50:07 +0300

    corrected the environment name

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-call "C:\Users\%USERNAME%\miniconda3\envs\qr_scanner_exp\python.exe" "%~dp0main.py"
+call "C:\Users\%USERNAME%\miniconda3\envs\qr_scanner\python.exe" "%~dp0main.py"
 pause
