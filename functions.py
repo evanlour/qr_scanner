5564efc5c616de1bd29e1b58958ac15ab6e61883
Evangelos Lourmpakis <evanlour@ee.duth.gr>
2024-06-06 21:35:38 +0300

    Working code, currenly goes under testing

--- functions.py
+++ functions.py
@@ -3,16 +3,16 @@ from pyzbar.pyzbar import decode, ZBarSymbol
 import pyqrcode
 import os
 
-def createQr(userIn):
-    qrcode = pyqrcode.create(userIn)
+def createQr(userIn): #This function uses text and converts it to qrcode
+    qrcode = pyqrcode.create(userIn) #We encode our data to a qr code
     out = "Data"
-    if not os.path.exists(out):
+    if not os.path.exists(out): #We check if the required directories exist
         os.makedirs(out)
     out = out + "\\Employees"
     if not os.path.exists(out):
         os.makedirs(out)
     out = out + "\\" + userIn + ".png"
-    qrcode.png(out, scale=10)
+    qrcode.png(out, scale=10) #We extract our qrcode as png
 
 def getQrCodeInfo(inputImage): #This function decodes the info the given QR has and returns it
     retData = "-1"
@@ -22,10 +22,10 @@ def getQrCodeInfo(inputImage): #This function decodes the info the given QR has 
             retData = data
     return retData
 
-def createUserId(Main, employeeFirstName, employeeLastName, employeeEmail, employeePhone):
+def createUserId(Main, employeeFirstName, employeeLastName, employeeEmail, employeePhone): #This function creates a new employee entity with the given data
     if not os.path.exists("Data\\Employees\\employees.csv"):
         createEmployeeTable()
-    df = pd.read_csv("Data\\Employees\\employees.csv")
+    df = pd.read_csv("Data\\Employees\\employees.csv") #We read the employee table
     df = df.set_index("employee_id")
     temp_df = df.loc[df['employee_first_name'] == employeeFirstName]
     if employeeLastName in temp_df.values:
@@ -39,20 +39,22 @@ def createUserId(Main, employeeFirstName, employeeLastName, employeeEmail, emplo
                      "employee_email" : employeeEmail,
                      "employee_phone" : employeePhone}])
     employeeInfo = employeeInfo.set_index('employee_id')
-    df = pd.concat([df, employeeInfo], axis=0)
+    df = pd.concat([df, employeeInfo], axis=0) #We add the employee to the main employee table
     createQr(employeeAlias)
-    Main.consoleLabel.addText("\nThe employee " + employeeFirstName + " " + employeeLastName + " has been added to the employees table")
+    Main.consoleLabel.addText("The employee " + employeeFirstName + " " + employeeLastName + " has been added to the employees table")
     df.to_csv('Data\\Employees\\employees.csv')
 
-def createDayTable(directory): #CSV blueprint for our data
+def createDayTable(directory): #CSV blueprint for our daily data
     columns = ['DateTime', 'employee_alias', 'employee_arrival', 'arrival_deadline', 'employee_departure', 'departure_deadline']
     df = pd.DataFrame(columns=columns)
     df = df.set_index("DateTime")
     df.to_csv(directory + ".csv")
     pass
 
-def createEmployeeTable():
+def createEmployeeTable(): #CSV blueprint for our employee table data
     columns = ["employee_id", "employee_first_name", "employee_last_name", "employee_alias", "employee_email", "employee_phone"]
     df = pd.DataFrame(columns=columns)
     df = df.set_index("employee_id")
+    if not os.path.exists("Data\\Employees"):
+        os.makedirs("Data\\Employees")
     df.to_csv("Data\\Employees\\employees.csv")
--- install.bat
+++ install.bat
@@ -1,10 +1,13 @@
 @echo off
 pause
 call C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat
-call conda env create --file test.yaml
-call conda activate qr_scanner_exp
+call conda env create --file environment.yaml
+call conda activate qr_scanner
 call pip install pyzbar
 call pip install qreader
-call pip install pyqt5-tools --pre
-call conda env update --file test.yaml
+call pip install pyqt5
+call pip install pyqt5-tools
+call pip install pyqt5designer
+call pip install pyqrcode
+call conda env update --file environment.yaml
 pause
